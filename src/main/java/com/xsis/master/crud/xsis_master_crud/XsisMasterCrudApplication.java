889c08ceb4fa86e68e16b34491de25d15ade48b1
muhlisahmad <muhllis.ahmad@gmail.com>
2024-10-27 23:23:59 +0700

    chore: project initialization

---
+++ src/main/java/com/xsis/master/crud/xsis_master_crud/XsisMasterCrudApplication.java
@@ -0,0 +1,13 @@
+package com.xsis.master.crud.xsis_master_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsisMasterCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XsisMasterCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xsis/master/crud/xsis_master_crud/XsisMasterCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsis.master.crud.xsis_master_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsisMasterCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
